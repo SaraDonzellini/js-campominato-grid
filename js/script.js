10dc8bcfbad1c78224b7d7f383225786bb03d3f5
SaraDonzellini <saradonzie@gmail.com>
2022-11-30 15:39:49 +0100

    fixes and changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 const buttonPlay = document.querySelector(".btn")
 
-buttonPlay.addEventListener('click', function() {
+buttonPlay.addEventListener('click', function () {
+  const divBoxElement = document.querySelector(".box")
+  let newSquare;
   for (let i = 0; i < 100; i++) {
-    const newSquare = document.createElement("div")
+    newSquare = document.createElement("div")
     newSquare.classList.add('square')
-    const divBoxElement = document.querySelector(".box")
     divBoxElement.appendChild(newSquare)
     newSquare.append(i + 1)
   }
-  
+
+
 })
